da93c998c48368dee53fb5b3fabcd80966f52677
Javier Gemar <javigemar@gmail.com>
2022-07-03 23:37:19 +0200

    Tkinter en Python - YouTube de Manuel González

--- Nivel 36 - Interfaces gráficas con Tkinter/reto_06.py
+++ Nivel 36 - Interfaces gráficas con Tkinter/reto_06.py
@@ -6,33 +6,13 @@ import tkinter
 ventana = tkinter.Tk()
 
 ventana.title('Mi aplicación')
-'''
-Al indicar la geometría podemos indicar las coordenadas de la ubicación de la ventana en pantalla, 
-para que siempre se muestre en la misma posición. Para esto indicamos después de las dimensiones 
-el parámetro del eje X y del eje Y separados con el signo más (+) de las dimensiones.
-'''
-# ventana.geometry('800x500+900+400')
-
-'''
-Para evitar la redimensión de la ventana usaríamos el siguiente código:
-ventana.resizable(width=False, height=False)
-también se podría poner así:
-ventana.resizable(False, False)
-En el caso de que queramos permitir la redimensión pondríamos True en lugar de False en el ancho o el alto.
-'''
-
-# ventana.minsize(width=400, height=250)      # Establecemos unos mínimos de redimensión
-# ventana.maxsize(width=1200, height=800)     # Establecemos unos máximos de redimensión
-# ventana.configure(background="grey")
 
 # DEFINIMOS FUNCIONES
 
-contador = 0
-
 def cuenta_numeros():
-    global contador
+    contador = int(numero['text'])              # Lo convertimos a tipo int, ya que está declarado como string.
     contador += 1
-    numero['text'] = contador
+    numero['text'] = contador                   # También se podría poner así: numero.config(text=contador)
 
 # CREACIÓN DE WIDGETS
 
@@ -49,32 +29,27 @@ rotulo_titulo = tkinter.Label(ventana,
                          relief='groove',       # Borde de la etiqueta, también: relief=tkinter.RAISED
                          border=10              # Tamaño del borde
                          )
-'''
-En lugar de los parámetros padx y pady para los márgenes podemos usar width y height, pero en lugar 
-de píxeles indicamos espacios. Hemos puesto 40 espacios en el eje X y 3 espacios en el eje Y. 
-Depende del tamaño de la fuente el tamaño de los espacios del eje X e Y.
-'''
-rotulo_titulo.pack()
+
+rotulo_titulo.pack(fill=tkinter.BOTH, expand=True, padx=20, pady=20, ipadx=20, ipady=20)
 
 numero = tkinter.Label(ventana,
                        text='0',
+                       # Podríamos definir también text=0 (directamente como int) y no necesitamos hacer la conversión.
                        font=('arial', 48),
-                       bg='darkgreen', fg='lightblue',
-                       padx=20, pady=20
+                       bg='darkgreen', fg='lightblue'
                        )
-numero.pack()
+numero.pack(fill=tkinter.BOTH, expand=True, padx=20, pady=20, ipadx=20, ipady=20)
 
 contar = tkinter.Button(ventana,
-                        text='contar',
+                        text='Contar',
                         font=('arial', 18),
                         bg='lightgrey', fg='black',
                         padx=20, pady=20,
                         relief=tkinter.GROOVE, bd=10,
                         command=cuenta_numeros
                         )
-contar.pack()
+contar.pack(fill=tkinter.BOTH, expand=True, padx=20, pady=20, ipadx=20, ipady=20)
 
-
-
+# BUCLE PRINCIPAL
 
 ventana.mainloop()
